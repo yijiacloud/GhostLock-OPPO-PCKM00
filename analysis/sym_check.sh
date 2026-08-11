4c6b90709e659b510dcb43bdad8704aceb2c9cdf
opporoot-research <opporoot.research@users.noreply.github.com>
2026-08-11 19:41:17 +0800

    remove ad-hoc analysis scripts

--- analysis/sym_check.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "=== mm_init / related symbols ==="
-grep -E ' mm_init$| do_basic_setup| kernel_init_freeable| start_kernel$| rest_init$| fork_init$' /mnt/d/opporoot/analysis/kallsyms_v2e.txt
-echo "=== mm_cachep context: symbols near it ==="
-grep -E 'mm_cachep|kmem_cache' /mnt/d/opporoot/analysis/kallsyms_v2e.txt | head
