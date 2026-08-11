ff7a64a39a10ccbc05abdedfe8eb1368ae531ffc
opporoot-research <opporoot.research@users.noreply.github.com>
2026-08-11 19:24:11 +0800

    remove ad-hoc analysis scripts

--- analysis/ks_check.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-grep -rn 'DIRECT_MAP_BASE\|DIRECT_MAP_END\|VMEMMAP' /mnt/d/opporoot/exploit/src/ | grep -v kernelsnitch | head -20
--- analysis/mm_scan.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-NDK=/opt/ndk/android-ndk-r29/toolchains/llvm/prebuilt/linux-x86_64
-# mm_init vaddr from kallsyms
-grep -E ' mm_init$' /mnt/d/opporoot/analysis/kallsyms_v2e.txt
-# text file offset = 0x20000. mm_init kallsyms? find it
