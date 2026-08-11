93e1e2663aaccd12e1261e71feb12a48e1e4243a
opporoot-research <opporoot.research@users.noreply.github.com>
2026-08-11 19:51:04 +0800

    remove ad-hoc analysis script

--- analysis/ks_check.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "=== constants ==="
-grep -n 'APPENDED_FUTEXES\|KERNELSNITCH_THRESHOLD_MULT\|MULITPLE\|futex_hash_table_size' /mnt/d/opporoot/exploit/src/kernelsnitch/kernelsnitch.h | head
-echo '=== find_collisions ==='
-sed -n '305,345p' /mnt/d/opporoot/exploit/src/kernelsnitch/kernelsnitch.h
